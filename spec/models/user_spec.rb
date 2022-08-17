3f1f3c918ce0107f58d88a0e1ed0ec613821b07c
takumikomatsu <takumitat@gmail.com>
2022-08-17 18:19:22 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -105,6 +105,11 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('First name kana には全角文字を使用してください')
       end
+      it '生年月日が空だと登録できない' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
     end
   end
 end
